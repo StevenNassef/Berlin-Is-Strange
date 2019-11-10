ca97299ce80163e3900e8687b307ddddb1a1ea94
Steven Henry <stevennassef97@gmail.com>
2019-11-11 00:49:46 +0200

    1- Initial Descision Class 2- Created a simple GameDecision

---
+++ Assets/_Scripts/ScriptableObjects/GameDecision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "GameDecision")]
+public class GameDecision : ScriptableObject
+{
+    [SerializeField] private string decisionQuestion;
+    public string DecisionQuestion { get { return decisionQuestion; } }
+    [SerializeField] private string firstDecision;
+    public string FirstDecision { get { return firstDecision; } }
+    [SerializeField] private string secondDecision;
+    public string SecondDecision { get { return secondDecision; } }
+
+    [SerializeField] private PlayableAsset firstDecisionCutScene;
+    public PlayableAsset FirstDecisionCutScene { get { return firstDecisionCutScene; } }
+    [SerializeField] private PlayableAsset secondDecisionCutScene;
+    public PlayableAsset SecondDecisionCutScene { get { return secondDecisionCutScene; } }
+}
