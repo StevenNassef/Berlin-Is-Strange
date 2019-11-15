299faeb61208e170a0b46387f9f2a10ec5fba74a
Steven Henry <stevennassef97@gmail.com>
2019-11-15 22:10:51 +0200

    1- Created MainMenu Timelines

---
+++ Assets/_Scripts/UI/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private PlayableAsset mainMenuIdle;
+    [SerializeField] private PlayableAsset charSelection;
+    [SerializeField] private PlayableAsset charDeSelection;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
